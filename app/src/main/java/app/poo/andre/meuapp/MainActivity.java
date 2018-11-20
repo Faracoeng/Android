bc514c53e6e3b0e104564fbc201f8f8881e33f29
Faracoeng <alfmazucheli@gmail.com>
2018-11-20 09:13:28 -0200

    aperfeiçoado

--- app/src/main/java/app/poo/andre/meuapp/MainActivity.java
+++ app/src/main/java/app/poo/andre/meuapp/MainActivity.java
@@ -1,5 +1,6 @@
 package app.poo.andre.meuapp;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,21 +10,39 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private int mContador = 0;
     private TextView mTextViewContador;
+    private TextView mShowCount;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+          super.onSaveInstanceState(outState);// salvando o valor que eta no TextView
+          outState.putInt("count", Integer.parseInt(String.valueOf(mShowCount.getText())));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.mTextViewContador = findViewById(R.id.TvContador);
+        this.mTextViewContador = findViewById(R.id.TvContador);// R.id é padrão para identificar o botão assiciado
+        mShowCount = findViewById(R.id.showTitle);
+        if (savedInstanceState != null) {
+            int count = savedInstanceState.getInt("count");
+            if (mShowCount != null) {
+                mShowCount.setText(count);
 
+            }
+        }
     }
 
+
+    //@SuppressLint("SetTextI18n")
     public void incrementarContador(View view) {
-       mContador++;
+        mContador++;
+        mTextViewContador.setText(Integer.toString(mContador));
 
-       mTextViewContador.setText(Integer.toString(mContador));
-
-        Toast toast = Toast.makeText(this,R.string.texto_saudacao,Toast.LENGTH_LONG);
+        Toast toast = Toast.makeText(this, "incrementou", Toast.LENGTH_LONG);
+                                                        //ou R.string.texto_saudacao
         toast.show();
+
     }
+
 }
---
+++ app/src/main/java/app/poo/andre/meuapp/SegundaActivity.java
@@ -0,0 +1,16 @@
+package app.poo.andre.meuapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+        Intent messageIntent = new Intent(this, SegundaActivity.class);
+        startActivity(messageIntent);
+    }
+}
