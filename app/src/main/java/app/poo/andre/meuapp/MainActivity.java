43706d095f0395a5c49de5e37567b2fc53dbad7a
Faracoeng <alfmazucheli@gmail.com>
2018-11-27 08:14:42 -0200

    27/11

--- app/src/main/java/app/poo/andre/meuapp/MainActivity.java
+++ app/src/main/java/app/poo/andre/meuapp/MainActivity.java
@@ -2,6 +2,7 @@ package app.poo.andre.meuapp;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,17 +13,28 @@ public class MainActivity extends AppCompatActivity {
     private int mContador = 0;
     private TextView mTextViewContador;
     private TextView mShowCount;
+    private static final String CONTADOR_PREFS = "contadorSharedPrefs";
+    private SharedPreferences mSharedPrefs;
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
           super.onSaveInstanceState(outState);// salvando o valor que eta no TextView
           outState.putInt("count", Integer.parseInt(String.valueOf(mShowCount.getText())));
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       //parte alterada ultima aula 27/11 --------------------------------
+        mSharedPrefs = getSharedPreferences(CONTADOR_PREFS,MODE_PRIVATE);
+        //pegar do disco o valor persistente no passado
+
+        mContador =mSharedPrefs.getInt("CONTADOR", 0);
+        this.mTextViewContador = findViewById(R.id.TvContador);
+        this.mTextViewContador.setText(mContador);
+        //----------------------------------------------------------------
         this.mTextViewContador = findViewById(R.id.TvContador);// R.id é padrão para identificar o botão assiciado
         mShowCount = findViewById(R.id.showTitle);
         if (savedInstanceState != null) {
@@ -34,7 +46,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor prefsEditor = mSharedPrefs.edit();
+        prefsEditor.putInt("Contador",mContador);
+        prefsEditor.apply();
+    }
 
     public void incrementarContador(View view) {
         mContador++;
